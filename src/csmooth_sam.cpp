dcb65af567491544277eece3f4a0709428664a62
Fabio Corradini <fabio.corradini@inia.cl>
2020-03-06 11:32:48 +0100

    fix uword vs int comparison in csmooth_sam

--- src/csmooth_sam.cpp
+++ src/csmooth_sam.cpp
@@ -6,6 +6,8 @@ arma::cube csmooth_sam(arma::cube myCube, int wind, int bins, int nslices){
 	// wind = window size.
 	// bins = how many clusters we had/have?
 	// nslices = how many slices, starting from zero, should be smoothered.
+	int max_drow = myCube.n_rows;
+  int max_dcol = myCube.n_cols;
 	
 	arma::cube tmp_Cube(size(myCube.slices(0, nslices)));
 	
@@ -13,21 +15,21 @@ arma::cube csmooth_sam(arma::cube myCube, int wind, int bins, int nslices){
 	{
 	  arma::mat myCubeSlice = myCube.slice(dslice);
 	  
-		for(int drow = 0; drow < myCubeSlice.n_rows; ++drow)
+		for(int drow = 0; drow < max_drow; ++drow)
 		{
-			for(int dcol = 0; dcol < myCubeSlice.n_cols; ++dcol)
+			for(int dcol = 0; dcol < max_dcol; ++dcol)
 			{
 				int first_row = (drow - wind > 0) ? drow - wind : 0;
 				int first_col = (dcol - wind > 0) ? dcol - wind : 0;
-				int last_row = (drow + wind < myCubeSlice.n_rows - 1) ? drow + wind : myCubeSlice.n_rows -1;
-				int last_col = (dcol + wind < myCubeSlice.n_cols - 1) ? dcol + wind : myCubeSlice.n_cols -1;
+				int last_row = (drow + wind < max_drow - 1) ? drow + wind : max_drow -1;
+				int last_col = (dcol + wind < max_dcol - 1) ? dcol + wind : max_dcol -1;
 
 				arma::vec electors = arma::vectorise(myCubeSlice.submat(first_row, first_col, last_row, last_col));
 
 				// find the mode
 				// we can do that using an histogram
 				std::vector<int> histogram(bins, 0);
-				for(int i = 0; i < electors.n_elem; ++i)
+				for(arma::uword i = 0; i < electors.n_elem; ++i)
 					++histogram[ electors[i] ];
 				int vote = std::max_element( histogram.begin(), histogram.end() ) - histogram.begin();
 
