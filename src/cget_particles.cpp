7c85ea2830a4719b1cf55f25e68baf5775f352ab
Fabio Corradini <fabio.corradini@inia.cl>
2020-03-09 10:01:04 +0100

    fix uword vs int comparison in cget_particles.cpp

--- src/cget_particles.cpp
+++ src/cget_particles.cpp
@@ -1,12 +1,15 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 
+// I think I don't use any of these functions anymore. As the summary is done by vectorizing the raster.
+// grep -rnwl R/ -e "cget_particles"  
+
 arma::mat buffer_add(int r, int c, int t_r, int t_c, arma::mat buffer, arma::mat A){
   arma::mat tmp_add(1,4);
   tmp_add(0) = A(r,c);
   tmp_add(2) = r;
   tmp_add(3) = c;
-  for(int i = 0; i < buffer.n_rows; ++i){
+  for(arma::uword i = 0; i < buffer.n_rows; ++i){
     if( buffer(i,2) == t_r && buffer(i,3) == t_c ){
       tmp_add(1) = buffer(i,1);
       buffer = join_cols(buffer, tmp_add);
@@ -31,14 +34,14 @@ arma::mat buffer_new(int r, int c, arma::mat buffer, arma::mat A){
 
 arma::mat buffer_merge(int r, int c, int t_r, int t_c, arma::mat buffer){
   arma::vec tmp_vec(2);
-  for( int i = 0; i < buffer.n_rows; ++i ){
+  for( arma::uword i = 0; i < buffer.n_rows; ++i ){
     if( buffer(i,2) == r && buffer(i,3) == c ){
       tmp_vec(0) = buffer(i,1);
     } else if( buffer(i,2) == t_r && buffer(i,3) == t_c ){
       tmp_vec(1) = buffer(i,1);
     }
   }
-  for( int i = 0; i < buffer.n_rows; ++i ){
+  for( arma::uword i = 0; i < buffer.n_rows; ++i ){
     if( buffer(i,1) == tmp_vec.max() ){
       buffer(i,1) = tmp_vec.min();
     }
@@ -68,8 +71,8 @@ arma::mat cget_particles(arma::mat A){
   buffer << -1 << -1 << -1 << -1 << arma::endr;
   // value; particle number; number of pixels
   
-  for(int r = 0; r < A.n_rows; ++r){
-    for(int c = 0; c < A.n_cols; ++c){
+  for(arma::uword r = 0; r < A.n_rows; ++r){
+    for(arma::uword c = 0; c < A.n_cols; ++c){
       
       if( r-1 >= 0 && c-1 >= 0 && c+1 < A.n_cols && ( A(r,c) == A(r-1,c-1) || A(r,c) == A(r,c-1) ) && A(r,c) != A(r-1,c) && A(r,c) == A(r-1,c+1) ){
         if( A(r,c) == A(r,c-1) ){
@@ -95,7 +98,8 @@ arma::mat cget_particles(arma::mat A){
         buffer = buffer_new(r,c,buffer,A);
       }
     }
-    if( r -1 == -1 ){
+    int r_test = r;
+    if( r_test -1 == -1 ){
       buffer.shed_row(0);
     }
   }
