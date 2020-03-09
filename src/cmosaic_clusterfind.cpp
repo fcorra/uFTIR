67846d5ada07a097566afdbbbab28067426e3869
Fabio Corradini <fabio.corradini@inia.cl>
2020-03-09 10:07:30 +0100

    fix uword vs int comparison in cmosaic_clusterfind.cpp

--- src/cmosaic_clusterfind.cpp
+++ src/cmosaic_clusterfind.cpp
@@ -4,11 +4,11 @@
 // [[Rcpp::export]]
 arma::cube cmosaic_clusterfind(arma::cube sam_match, arma::rowvec clusters){
 	arma::cube out(size(sam_match));
-	for(int s = 0; s < out.n_slices; ++s)
+	for(arma::uword s = 0; s < out.n_slices; ++s)
 	{
-		for(int r = 0; r < out.n_rows; ++r)
+		for(arma::uword r = 0; r < out.n_rows; ++r)
 		{
-			for(int c = 0; c < out.n_cols; ++c)
+			for(arma::uword c = 0; c < out.n_cols; ++c)
 			{
 				out(r,c,s) = clusters(sam_match(r,c,s) -1);
 			}
